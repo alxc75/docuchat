61ca44753e3e8a82647cd7068125abe3acbc2a55
xTellarin <alexis.cappe@me.com>
2023-12-10 18:35:50 +0100

    Added GNU GPL V3 License

--- pages/settings.py
+++ pages/settings.py
@@ -33,3 +33,22 @@ if new_api_key != api_key and new_api_key.strip() != "":
     with open("userinfo.json", "w") as f:
         json.dump(userinfo, f, indent=4)
     st.rerun()  # Force a rerun to reload the updated userinfo.json
+
+
+
+# ------------------- LICENSE -------------------
+# Docuchat, a smart knowledge assistant for your documents.
+# Copyright © 2023 xTellarin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/.
